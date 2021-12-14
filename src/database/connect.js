735793c0f06183ed4ac109941f314d9a27153c4f
tminhbao <tminhbao2001@gmail.com>
2021-12-14 10:04:44 +0700

    Cập nhật lại Host Database (Server)

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,17 +1,16 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const conn = mysql.createConnection({
-  connectionLimit: 10,
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6456787',
-  password: 'VRPL3AsQP4',
-  database: 'sql6456787',
-  charset: 'utf8_general_ci',
-  port: '3306'
+  host: "db4free.net",
+  user: "minhbao",
+  password: "123456789",
+  database: "laptop_webshop",
+  charset: "utf8_general_ci",
+  port: "3306",
+  multipleStatements: true,
 });
 
 conn.connect(function (err) {
-  if (err)
-    throw err;
-})
+  if (err) throw err;
+});
 
 module.exports = conn;
