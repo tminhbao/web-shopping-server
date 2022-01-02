8d620ecc3e3eebd830328e0e3f990566a4f94b93
tminhbao <tminhbao2001@gmail.com>
2022-01-02 10:25:33 +0700

    Fix lỗi Đăng ký, Đăng nhập

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 const conn = mysql.createConnection({
-  host: "db4free.net",
-  user: "minhbao",
-  password: "123456789",
-  database: "laptop_webshop",
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6461859",
+  password: "LAbxYqUQVd",
+  database: "sql6461859",
   charset: "utf8_general_ci",
-  port: "3306",
+  port: 3306,
   multipleStatements: true,
 });
 
