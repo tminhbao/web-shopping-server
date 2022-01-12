d503b0ef029c3de4ed59d3a8e9355f6257d7c15b
tminhbao <tminhbao2001@gmail.com>
2022-01-12 22:53:45 +0700

    Thêm chức năng: Admin thay đổi thông tin cá nhân chính mình

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,26 @@
-const homeRouter = require('./home');
-const orderRouter = require('./order');
-const productRouter = require('./product');
-const reportRouter = require('./report');
-const usersRouter = require('./users');
-const signinRouter = require('./signin');
-const signupRouter = require('./signup');
-const logoutRouter = require('./logout');
-const accountRouter = require('./account')
-const accoutListRouter = require('./accountlist')
+const homeRouter = require("./home");
+const orderRouter = require("./order");
+const productRouter = require("./product");
+const reportRouter = require("./report");
+const usersRouter = require("./users");
+const signinRouter = require("./signin");
+const signupRouter = require("./signup");
+const logoutRouter = require("./logout");
+const accountRouter = require("./account");
+const accoutListRouter = require("./accountlist");
+const editInfoRouter = require("./editinfo");
 
 function route(app) {
-    app.use('/signin',signinRouter);
-    app.use('/signup',signupRouter);
-    app.use('/order',orderRouter);
-    app.use('/product',productRouter);
-    app.use('/report',reportRouter);
-    app.use('/user',usersRouter);
-    app.post('/logout',logoutRouter);
-    app.use('/account',accountRouter);
-    app.use('/accountlist',accoutListRouter);
-    app.use('/',homeRouter);
+  app.use("/signin", signinRouter);
+  app.use("/signup", signupRouter);
+  app.use("/order", orderRouter);
+  app.use("/product", productRouter);
+  app.use("/report", reportRouter);
+  app.use("/user", usersRouter);
+  app.post("/logout", logoutRouter);
+  app.use("/account", accountRouter);
+  app.use("/accountlist", accoutListRouter);
+  app.use("/editinfo", editInfoRouter);
+  app.use("/", homeRouter);
 }
 module.exports = route;
