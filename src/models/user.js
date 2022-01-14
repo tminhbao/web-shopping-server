912c3eafb4adfd6faba4abe351e4da1117ed2063
tminhbao <tminhbao2001@gmail.com>
2022-01-14 15:36:53 +0700

    Phân trang danh sách Admin

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,25 @@
-const db = require('../database/connect');
+const db = require("../database/connect");
+const ITEM_PER_PAGE = 10;
 
-const listUser = [];
-const getList = () => {
-    let sql = `SELECT * FROM admins`
-    db.query(sql, (err, result) => {
-        if (err)
-            throw err;
-        return result;
-    })
-}
+const executeQuery = (query) => {
+  return new Promise((res, rej) => {
+    db.query(query, (err, result) => {
+      if (err) rej(err);
+      else res(result);
+    });
+  });
+};
 
-module.exports = { getList }
+const getListAdmin = async (page) => {
+  const offset = (page - 1 || 1 - 1) * ITEM_PER_PAGE;
+  const sqlPaginate = `SELECT * FROM admins LIMIT ${ITEM_PER_PAGE} OFFSET ${offset}`;
+  const sqlTotalItem = `SELECT COUNT(*) AS totalItem FROM admins`;
+  const [listItem, totalItem] = await Promise.all([
+    executeQuery(sqlPaginate),
+    executeQuery(sqlTotalItem),
+  ]);
+  const totalPage = Math.ceil((totalItem[0].totalItem || 0) / ITEM_PER_PAGE);
+  return { listItem, totalPage, page };
+};
+
+module.exports = { getListAdmin };
