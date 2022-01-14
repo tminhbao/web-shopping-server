341ec08fcdb851fe39675731962026d866378a89
tminhbao <tminhbao2001@gmail.com>
2022-01-14 15:39:33 +0700

    Phân trang danh sách người dùng

--- src/models/user.js
+++ src/models/user.js
@@ -22,4 +22,16 @@ const getListAdmin = async (page) => {
   return { listItem, totalPage, page };
 };
 
-module.exports = { getListAdmin };
+const getListUser = async (page) => {
+  const offset = (page - 1 || 1 - 1) * ITEM_PER_PAGE;
+  const sqlPaginate = `SELECT * FROM users LIMIT ${ITEM_PER_PAGE} OFFSET ${offset}`;
+  const sqlTotalItem = `SELECT COUNT(*) AS totalItem FROM users`;
+  const [listItem, totalItem] = await Promise.all([
+    executeQuery(sqlPaginate),
+    executeQuery(sqlTotalItem),
+  ]);
+  const totalPage = Math.ceil((totalItem[0].totalItem || 0) / ITEM_PER_PAGE);
+  return { listItem, totalPage, page };
+};
+
+module.exports = { getListAdmin, getListUser };
