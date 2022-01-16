c7f6c9df171de3e139012f19ac3728f7a3be1a80
Trần Cường <trancuongitus@gmail.com>
2022-01-17 00:12:47 +0700

    Update product model

--- src/models/product.js
+++ src/models/product.js
@@ -56,24 +56,4 @@ const getLaptopById = async (id) => {
     })
 }
 
-// exports.add = (data) => {
-//     var values = [
-//         [data.laptop_id, data.manufacture, data.model, data.laptop_name, '', data.laptop_cpu, data.laptop_ram, data.laptop_vga, data.laptop_disk, data.image, data.cost, data.price, data.inventory]
-//     ]
-
-//     let insert_laptop_sql = 'INSERT INTO laptop(laptop_id, manufacture, model, laptop_name, laptop_description, laptop_cpu, laptop_ram, laptop_vga, laptop_disk, image, cost, price, inventory) VALUES ?'
-//     let query = db.query(insert_laptop_sql, [values], (err, result) => {
-//         if (err) throw err;
-//         console.log("Number of records inserted: " + result.affectedRows);
-//     })
-// }
-
-// exports.delete = (id) => {
-//     let delete_laptop_sql = 'DELETE FROM laptop WHERE laptop_id = ?';
-//     let query = db.query(delete_laptop_sql, id, (err, result) => {
-//         if (err) throw err;
-//         console.log("Number of records deleted: " + result.affectedRows);
-//     })
-// }
-
 module.exports = { getList, add, remove, getLaptopById }
